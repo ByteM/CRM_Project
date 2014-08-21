b6acd6c70065905679a263910ab16c1cbcafdb46
lalitbadule <lalitbadule@gmail.com>
2014-08-21 17:25:54 +0530

    New change

---
+++ CRM_Project/CRM_DAL/DAL_CheckClearUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using CRM_BAL;
+
+namespace CRM_DAL
+{
+    public class DAL_CheckClearUpdate
+    {
+    }
+}
